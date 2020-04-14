5429928ae35864fa4ff50cbcda3b5d83f3ce33c3
xinzezhang <xinze@hust.edu.cn>
2020-04-14 23:35:05 +0800

    update

--- experiment/train-oracle.sh
+++ experiment/train-oracle.sh
@@ -7,8 +7,10 @@ CUDA_VISIBLE_DEVICES=2,3 fairseq-train \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 4096 \
     --eval-bleu \
-    --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+    --eval-bleu-args '{"beam": 10, "max_len_a": 1.2, "max_len_b": 20}' \
     --eval-bleu-detok moses \
     --eval-bleu-remove-bpe \
     --eval-bleu-print-samples \
-    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric 
+    --best-checkpoint-metric bleu \
+    --no-epoch-checkpoints \
+    --maximize-best-checkpoint-metric 
