e43276b777b6877881b23cd92dc91a1acc7713b1
xinzezhang <xinze@huste.edu.cn>
2021-01-14 14:53:56 +0000

    run transformer bpe

--- data-src/prep-ldc-forward.bpe.sh
+++ data-src/prep-ldc-forward.bpe.sh
@@ -1,80 +1,79 @@
 echo 'prepare LDC and NIST bpe dataset'
 
-# SCRIPTS=experiment/translation/mosesdecoder/scripts
+# SCRIPTS=data-src/translation/mosesdecoder/scripts
 # TOKENIZER=$SCRIPTS/tokenizer/tokenizer.perl
 # LC=$SCRIPTS/tokenizer/lowercase.perl
 # CLEAN=$SCRIPTS/training/clean-corpus-n.perl
-BPEROOT=experiment/translation/subword-nmt/subword_nmt
+BPEROOT=data-src/translation/subword-nmt/subword_nmt
 BPE_TOKENS=60000
 
 src=cn
 tgt=en
 lang=cn-en
-task=forward.bpe.join
-bpe=experiment/ldc/$task
-word=experiment/ldc/$task/word
+task=forward.bpe
+bpe=data-src/ldc/$task
+word=data-src/ldc/$task/word
 
-# mkdir -p $bpe $word
+mkdir -p $bpe $word
 
-# echo "pre-processing train data..."
-# # using ldc  as training data of oracle model
-# for l in $src $tgt; do
-#     raw=experiment/ldc/ldc_raw/train.cn-en.$l
-#     cat $raw > $word/train.$l
-# done
+echo "pre-processing train data..."
+# using ldc  as training data of oracle model
+for l in $src $tgt; do
+    raw=data-src/ldc/ldc_raw/train.cn-en.$l
+    cat $raw > $word/train.$l
+done
 
-# echo "pre-processing valid data..."
-# #  using nist02 as validation data of forward model
-# nist02cn=experiment/ldc/ldc_raw/nist02/nist02.cn
-# cat $nist02cn > $word/valid.cn
-# nist02en=experiment/ldc/ldc_raw/nist02/nist02.en
-# cat ${nist02en}0 > $word/valid.en
-# for i in 0 1 2 3; do
-#     nist02eni=${nist02en}$i
-#     cat $nist02eni > $word/valid.en$i
-# done
+echo "pre-processing valid data..."
+#  using nist02 as validation data of forward model
+nist02cn=data-src/ldc/ldc_raw/nist02/nist02.cn
+cat $nist02cn > $word/valid.cn
+nist02en=data-src/ldc/ldc_raw/nist02/nist02.en
+cat ${nist02en}0 > $word/valid.en
+for i in 0 1 2 3; do
+    nist02eni=${nist02en}$i
+    cat $nist02eni > $word/valid.en$i
+done
 
-# echo "pre-processing test data..."
-# #  using nist-avg as test data of forward model
-# nist_avg_cn=experiment/ldc/ldc_raw/avg/avg.cn
-# cat $nist_avg_cn > $word/test.cn
-# nist_avg_en=experiment/ldc/ldc_raw/avg/avg.en
-# cat ${nist_avg_en}0 > $word/test.en
-# for i in 0 1 2 3; do
-#     nist_avg_eni=${nist_avg_en}$i
-#     cat $nist_avg_eni > $word/test.en$i
-# done
+echo "pre-processing test data..."
+#  using nist-avg as test data of forward model
+nist_avg_cn=data-src/ldc/ldc_raw/avg/avg.cn
+cat $nist_avg_cn > $word/test.cn
+nist_avg_en=data-src/ldc/ldc_raw/avg/avg.en
+cat ${nist_avg_en}0 > $word/test.en
+for i in 0 1 2 3; do
+    nist_avg_eni=${nist_avg_en}$i
+    cat $nist_avg_eni > $word/test.en$i
+done
 
-# echo "bpe train, valid, test..."
-# TRAIN=$word/train.cn-en
-# rm -f $TRAIN
+echo "bpe train, valid, test..."
+TRAIN=$word/train.cn-en
+rm -f $TRAIN
 
-# for l in $src $tgt; do
-#     cat $word/train.$l >> $TRAIN
-# done
+for l in $src $tgt; do
+    cat $word/train.$l >> $TRAIN
+done
 
-# BPE_CODE=$bpe/code
+BPE_CODE=$bpe/code
 
-# echo "learn_bpe.py on ${TRAIN}..."
-# python $BPEROOT/learn_bpe.py -s $BPE_TOKENS < $TRAIN > $BPE_CODE
+echo "learn_bpe.py on ${TRAIN}..."
+python $BPEROOT/learn_bpe.py -s $BPE_TOKENS < $TRAIN > $BPE_CODE
 
 
-# for L in $src $tgt; do
-#     for f in train.$L valid.$L test.$L; do
-#         echo "apply_bpe.py to ${f}..."
-#         python $BPEROOT/apply_bpe.py -c $BPE_CODE < $word/$f > $bpe/$f
-#     done
-# done
+for L in $src $tgt; do
+    for f in train.$L valid.$L test.$L; do
+        echo "apply_bpe.py to ${f}..."
+        python $BPEROOT/apply_bpe.py -c $BPE_CODE < $word/$f > $bpe/$f
+    done
+done
 
-TEXT=experiment/ldc/$task
+TEXT=data-src/ldc/$task
 fairseq-preprocess --source-lang cn --target-lang en \
     --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test \
     --destdir data-bin/ldc.$task.cn-en \
-    --workers 16 \
-    --joined-dictionary
+    --workers 16 
 
-# for i in 0 1 2 3; do
-#     nist_avg_eni=${nist_avg_en}$i
-#     echo "apply_bpe.py to ${nist_avg_eni}..."
-#     python $BPEROOT/apply_bpe.py -c $BPE_CODE < $nist_avg_eni > $bpe/test.en$i
-# done
+for i in 0 1 2 3; do
+    nist_avg_eni=${nist_avg_en}$i
+    echo "apply_bpe.py to ${nist_avg_eni}..."
+    python $BPEROOT/apply_bpe.py -c $BPE_CODE < $nist_avg_eni > $bpe/test.en$i
+done
--- train.transformer.sh
+++ train.transformer.sh
@@ -1,46 +1,28 @@
 export CUDA_VISIBLE_DEVICES=0,1
-data_bin_dir=./data-bin/ldc.forward.bpe.join.cn-en
-model_dir=./data-bin/ldc.forward.bpe.join.cn-en
-model_name=transformer.join
+
+# data_bin_dir=./data-bin/ldc.forward.bpe.join.cn-en
+# model_dir=./data-bin/ldc.forward.bpe.join.cn-en
+# model_name=transformer.join
+
+data_bin_dir=./data-bin/ldc.forward.bpe.cn-en
+model_dir=$data_bin_dir
+model_name=transformer
+
 
 python train.py $data_bin_dir --fp16\
-    --arch transformer_vaswani_wmt_en_de_big --share-all-embeddings \
-    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr-scheduler inverse_sqrt \
-    --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.0007 \
-    --weight-decay 0.0 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
-    --max-tokens 4096 --update-freq 3 --log-format json --max-epoch 40 \
-    --log-interval 10 --save-interval 2 --keep-last-epochs 10 \
-    --seed 1111 \
+    --arch transformer --share-decoder-input-output-embed \
+    --optimizer adam --adam-betas '(0.9, 0.998)' --clip-norm 0.0 \
+    --lr 1.0 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+    --dropout 0.4 --weight-decay 0.0001 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --max-tokens 4096 \
     --eval-bleu \
-    --eval-bleu-args '{"beam": 10}' \
+    --eval-bleu-args '{"beam": 5}' \
     --eval-bleu-detok moses \
+    --eval-bleu-remove-bpe \
     --eval-bleu-print-samples \
     --best-checkpoint-metric bleu \
     --no-epoch-checkpoints \
     --maximize-best-checkpoint-metric \
+    --patience 5 \
     --source-lang cn --target-lang en --save-dir $model_dir/$model_name | tee -a $model_dir/${model_name}/training.log 
-    
-#    --distributed-port 32222 --distributed-world-size 2 --ddp-backend=no_c10d
-
-# data_bin_dir=./data-bin/ldc.forward.word.cn-en
-# model_dir=./data-bin/ldc.forward.word.cn-en
-# # model_name=lstm
-# model_name=transformer
-
-# python train.py $data_bin_dir \
-#     --arch lstm_luong_wmt_en_de \
-#     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr-scheduler inverse_sqrt \
-#     --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.0007 --min-lr 1e-09 \
-#     --weight-decay 0.0 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
-#     --max-tokens 4096 --update-freq 3 --log-format json --max-epoch 30 \
-#     --log-interval 10 --save-interval 2 --keep-last-epochs 10 \
-#     --seed 1111 \
-#     --eval-bleu \
-#     --eval-bleu-args '{"beam": 10, "max_len_a": 1.2, "max_len_b": 20}' \
-#     --eval-bleu-detok moses \
-#     --eval-bleu-print-samples \
-#     --best-checkpoint-metric bleu \
-#     --no-epoch-checkpoints \
-#     --maximize-best-checkpoint-metric \
-#     --source-lang cn --target-lang en --save-dir $model_dir/$model_name | tee -a $model_dir/${model_name}/training.log 
-
